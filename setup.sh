5bdf7feb789871607d12b57143033c9ec664a6ef
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2023-11-19 18:33:15 +0000

    feat: script de preparação local.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Instalação das dependências
+pip install -r requirements.txt
+
+# Inicialização do aplicativo Flask
+export FLASK_APP=run.py
+export FLASK_ENV=development
+
+# Execução do aplicativo Flask
+flask run
